39bd68151719e61084a0455a943e112c0ceb93d5
FantastOkunev <okunev229@yandex.ru>
2021-04-23 00:26:48 +0300

    version 2

--- task.py
+++ task.py
@@ -1,5 +1,5 @@
 from imageio import imread, imwrite
-
+from math import ceil
 import numpy as np
 
 N = 5
@@ -8,12 +8,28 @@ im = imread('putin.bmp')
 
 h = len(im)
 w = len(im[0])
-print('width=', w, 'height=', h, range(5), 1//5, 5//5)
+print('width=', w, 'height=', h)
 im1 = np.empty((h*N, w*N, 3), dtype=np.uint8)
+im2 = np.empty((ceil(h/N)-1, ceil(w/N)-1, 3), dtype=np.uint8)
 
 for ix in range(h*N):
     for iy in range(w*N):
         im1[ix][iy] = im[ix//N][iy//N]
 
+for ix in range(ceil(h/N)-1):
+    for iy in range(ceil(w/N)-1):
+        sum = [0, 0, 0]
+        for k_ix in range(ix*N, ix*N+N):
+            for k_iy in range(iy*N, iy*N+N):
+                sum[0] += im[k_ix][k_iy][0]
+                sum[1] += im[k_ix][k_iy][1]
+                sum[2] += im[k_ix][k_iy][2]
+        sum[0] //= N*N
+        sum[1] //= N*N
+        sum[2] //= N*N
+
+        im2[ix][iy] = sum
+
 
 imwrite('face.bmp', im1)
+imwrite('face2.bmp', im2)
